25d59db1dc4726be6d7a644d30a4bc5c775fc404
dbo-Crypto <damienbourgery@gmail.com>
2023-01-18 16:51:32 +0100

    commit

--- scripts/contact-form.js
+++ scripts/contact-form.js
@@ -1,6 +1,24 @@
 function getContactFormData() {
-    document.querySelector('contactForm').addEventListener('submit', function (e) {
-        alert("hello");
-    });
+  let mail = document.getElementById("mail").value;
+  let sujet = document.getElementById("sujet").value;
+  let message = document.getElementById("message").value;
+  alert(mail)
 
+  Email.send({
+    SecureToken: "5021fe60-2829-47a0-b75d-416ef87dee88",
+    To: "contact.1e43r@simplelogin.com",
+    From: "contact.1e43r@simplelogin.com",
+    Subject: "BCS Demande de contact",
+    Body: "<html><h2 text-align=center>Demande de contact</h><h4>Demandeur: "+mail+"</h4><br><strong>Sujet: "+ sujet +"</strong><br>><p>Message:</p><p>"+ message +"</p></html>",
+  }).then((message) => alert("Message envoyé"));
 }
+
+/*
+contact.1e43r@simplelogin.com
+smtp.elasticemail.com
+Port: 2525
+A3C756A88747D8641EE112ADF45BEC4ACE97
+
+
+5021fe60-2829-47a0-b75d-416ef87dee88
+*/
